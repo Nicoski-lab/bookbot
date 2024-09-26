ea0b0a32a6aed35b817878b31080308d4d51468c
nicolas.rudowski <rudowski.nicolas@gmail.com>
2024-09-26 03:03:10 +0200

    Cleaned up code

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-def word_counter (text):
-    return(len(text.split()))
+def word_counter(text):
+    return len(text.split())
 
-def character_counter (text):
+def character_counter(text):
     characters_dict = {}
     lowered_text = text.lower()
-    letters = list(lowered_text)
-    for char in letters:
-        if char not in characters_dict:
-            characters_dict[char] = 1
-        else:
-            characters_dict[char] += 1
+    for char in lowered_text:
+        if char.isalpha():
+            characters_dict[char] = characters_dict.get(char, 0) + 1
     return characters_dict
 
 def get_amount(dict_item):
     return dict_item['amount']
 
-def sort_on (characters_dict):
-    letters_list = []
-    for letter, amount in characters_dict.items():
-        if letter.isalpha():
-            letters_list.append({"letter":letter, "amount":amount})
+def sort_characters(characters_dict):
+    letters_list = [{"letter": letter, "amount": amount} 
+                    for letter, amount in characters_dict.items()]
     letters_list.sort(reverse=True, key=get_amount)
     return letters_list
 
 def main():
     with open("books/frankenstein.txt") as f:
-            file_contents = f.read()
+        file_contents = f.read()
     return file_contents    
 
 if __name__ == "__main__":
     text = main()
     char_dict = character_counter(text)
     word_count = word_counter(text)
-    sorted_chars = sort_on(char_dict)
+    sorted_chars = sort_characters(char_dict)
 
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{word_count} words found in the document\n")
@@ -41,6 +36,3 @@ if __name__ == "__main__":
         print(f"The '{char_info['letter']}' character was found {char_info['amount']} times")
 
     print("--- End report ---")
-    
-
-
